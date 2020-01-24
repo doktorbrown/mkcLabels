ca2bc2bca175a70d737d1b76e22683d7bc81365a
doktorbrown <doktorbrown@users.noreply.github.com>
2020-01-24 17:40:30 -0500

    Update printers.py
    Adds a delay and preview of the label to be printed.  User is then prompted to enter the printer number which will be added to label.  .csv log now has printer number and timestamp

--- printers.py
+++ printers.py
@@ -1,15 +1,16 @@
 '''
 Created on Oct 12, 2018
 
-Modified on May 22,2019
+Modified on Jan 24,2020
 @author doktorbrown
 '''
-import os
+import os 
 import re
 import time
 from datetime import date
 import csv
 from bs4 import BeautifulSoup 
+from time import strftime
 #installation at http://www.crummy.com/software/BeautifulSoup/     $ pip install beautifulsoup4
 
 
@@ -22,11 +23,21 @@ campus = str()
 #the Q&A on the printers page has to be made fullscreen in the browser window and then saved as full html
 # this script is then run from the same directory the html was saved in via terminal
 
+
+#delayed start to allow html download to complete
+for d in range(1,10000000):
+    dd=d+1
+
+print("generating preview")
+
+ 
+
+
 try:
     fname ="3DPrinterOS - Printers.html"
     
 except: 
-    fname = "3DPrinterOS - Dashboard.html"
+    fname = "3DPrinterOS - Printers.html"
 soup = BeautifulSoup(open(fname), 'html.parser') 
 out = "clipboard.txt"
 log = "log.txt"
@@ -44,9 +55,15 @@ lastNameGetter = soup.find_all(text = re.compile("^What is your name?"))[0].next
 emailGetter = soup.find_all(text = re.compile("^Please re-enter your PSU email address."))[0].next.next
 # print(emailGetter)
 campusGetter = soup.find_all(text = re.compile("^Campus:"))[0].next.next
-# print campusGetter
+# print(campusGetter)
 
 today =str(date.today())
+# whatTime = str(time.localtime())
+# print(whatTime[3],":",whatTime[4])
+
+hour=strftime("%H:%M:%S")
+# print(hour)
+
 # print today
 # 
 # don't add campus if UP
@@ -57,22 +74,42 @@ else:
     campus = campusGetter
           
 # print to screen as verification
-print campus
-print lastNameGetter,",", firstNameGetter
-print today
-print emailGetter
+print (campus)
+print (lastNameGetter,",", firstNameGetter)
+print (today)
+print (emailGetter)
+
+
+#confirm preview and enter printer number
+
+print("please enter printer number and press return if preview is correct")
+
+printerNumber = int(input())
+
+if printerNumber =="n" or "N":
+    quit
+    
+else:
+    pass
+
+printerNumber=str(printerNumber)
+print("Printed on ", printerNumber)
+
+
+
+
 
 # send to clipboard
 # open output file for writing results
 f = open(out, 'w')
 #      
-#shorter for new labels
+#shorter for new labels 
 lineOne = (campus, '\n',
            lastNameGetter," ,", firstNameGetter,'\n',
            today, '\n',
            emailGetter,  '\n',
+           "Printer: ",printerNumber, '\n',
            "Consultation Scheduling:", '\n',
-           "makercommons@psu.edu", '\n',
            "makercommons.psu.edu")
 #  
 # #strip extra mc label info so it doesn't clutter log
@@ -92,9 +129,10 @@ os.system("lpr -o landscape -P DYMO_LabelWriter_450_Turbo clipboard.txt")
 
 # # open log file for appending results
 
-logRowEntry = (today, campus, lastNameGetter, firstNameGetter, emailGetter,'\n')
+logRowEntry = (today, campus, lastNameGetter, firstNameGetter, emailGetter, printerNumber,hour,'\n')
 
 csvLogs = csv.writer(open(csvLogOutput, 'a')) 
 csvLogs.writerow(logRowEntry)
 # if  log appended message appears, then everything has been written correctly to label and logs
-print "log appended"
+print (lineOne)
+print ("log appended")
